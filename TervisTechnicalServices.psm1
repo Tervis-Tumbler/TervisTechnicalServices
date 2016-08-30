5c2dbe12290ce0e9f36e5581f1b0b4340ca59aeb
ChrisMagnuson <hourglasssand@gmail.com>
2016-08-30 15:21:45 -0400

    Initial commit

---
+++ TervisTechnicalServices.psm1
@@ -0,0 +1,11 @@
+﻿function New-TervisEmployee {
+    param(
+        $GivenName,
+        $SurName,
+        $EmployeeID,
+        $EmployeeIDOfExistingEmployeeToModelPermissionsAfter,
+        [Switch]$Mac,
+        [Switch]$Laptop,
+        [Switch]$DualMonitors
+    )
+}
