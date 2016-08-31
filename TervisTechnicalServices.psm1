075f4ea3875ae0ab31eaf80fdc339a3bf498f505
fhoseinih <hosseini@tervis.com>
2016-08-31 14:50:00 -0400

    Add command to remove DN from telephoneNumber field in AD.

--- TervisTechnicalServices.psm1
+++ TervisTechnicalServices.psm1
@@ -18,5 +18,7 @@ function Invoke-TervisVOIPTerminateUser {
     )
 
     Invoke-TervisCUCMTerminateUser -UserName $SamAccountName
-    Invoke-TervisCUCTerminateVM -Alias $SamAccountName    
+    Invoke-TervisCUCTerminateVM -Alias $SamAccountName 
+    Set-ADUser $SamAccountName -OfficePhone $null
+       
 }
