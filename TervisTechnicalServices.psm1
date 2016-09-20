df30526ced288653e6a533787fdf6eb23bb91202
fhoseinih <hosseini@tervis.com>
2016-09-20 13:09:57 -0400

    Added New-TervisVOIPUser function.

--- TervisTechnicalServices.psm1
+++ TervisTechnicalServices.psm1
@@ -22,3 +22,26 @@ function Invoke-TervisVOIPTerminateUser {
     Set-ADUser $SamAccountName -OfficePhone $null
        
 }
+
+Function New-TervisVOIPUser {
+    param (
+        [ValidateSet("CallCenterAgent")]$UserType,
+        $UserID
+    )
+
+    if ($UserType -eq "CallCenterAgent") {
+        $Pattern = Find-CUCMLine -Pattern 7% -Description "" | select -First 1
+        Set-ADUser $UserID -OfficePhone $Pattern
+        Sync-CUCMtoLDAP
+
+        do {
+            sleep -Seconds 3
+        } until (Get-CUCMUser -UserID $UserID -ErrorAction SilentlyContinue)
+
+        $ADUser = Get-ADUser $UserID
+        $DisplayName = $ADUser.name
+
+        Set-CUCMLine -Pattern $Pattern -RoutePartitionName UCCX_PT -Description $DisplayName -AlertingName $DisplayName -AsciiAlertingName $DisplayName 
+
+    }
+}
